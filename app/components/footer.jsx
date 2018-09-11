9a5b255d3e5da5325f2b5abbfe26fbdd91a8dffa
gilardh <gilard@yahoo.com>
2018-09-11 23:42:06 +0200

    Update of the link od Discord on 'footer.jsx' file.

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -25,7 +25,7 @@ export default class ZFooter extends React.Component {
 
             <p>Keys are validated client-side and do not leave your browser or network. You are responsible for keeping your own keys safe!!!</p>
 
-            <p>Suggestions? <a href="https://myhush.org/#contact-section">Contact us</a> or join our <a href="http://myhush.org/discord">Discord</a> server!</p>          
+            <p>Suggestions? <a href="https://myhush.org/#contact-section">Contact us</a> or join our <a href="http://myhush.org/discord/">Discord</a> server!</p>          
 
             <p style={longP}>Donations are always welcome!<br/>
               <b>HUSH</b>: <a href="http://dashboard.myhush.org/#donate">Dashboard donation addresses</a><br/>
