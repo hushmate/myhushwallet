39d40d9c5945d1d565d17538fa6c1b55c6a91665
gilardh <gilardh@yahoo.com>
2018-11-16 22:13:12 +0100

    First refresh commit

---
+++ src/components/pages/Contact.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+export default class ZContact extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+          <h2>Contact</h2>
+            <p>
+              <b>JOIN US!</b>
+              <br/>
+              <br/>
+              <b>Hush</b> is a community project. Everyone is welcomed with open arms!
+            </p>
+
+            <p>
+              <br/>
+              <b>Add logo of Website, Discord, Twitter, Reddit, Bitcoin Talk and GitHub</b>
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
