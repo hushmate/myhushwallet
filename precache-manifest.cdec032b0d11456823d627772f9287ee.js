88fd6e546eda29c2968c96e7d3950cb0a13ffb63
gilardh <gilardh@yahoo.com>
2018-11-19 22:28:45 +0100

    Updates

---
+++ precache-manifest.cdec032b0d11456823d627772f9287ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46a337f0ffe419ca5f5d",
+    "url": "/myhushwallet/static/js/runtime~main.46a337f0.js"
+  },
+  {
+    "revision": "41ec62c6047f8556787a",
+    "url": "/myhushwallet/static/js/main.41ec62c6.chunk.js"
+  },
+  {
+    "revision": "089d43787393ae29e939",
+    "url": "/myhushwallet/static/js/1.089d4378.chunk.js"
+  },
+  {
+    "revision": "41ec62c6047f8556787a",
+    "url": "/myhushwallet/static/css/main.9679771b.chunk.css"
+  },
+  {
+    "revision": "089d43787393ae29e939",
+    "url": "/myhushwallet/static/css/1.336fd1cd.chunk.css"
+  },
+  {
+    "revision": "e55cad413af8e4f1285e4853a2b428d8",
+    "url": "/myhushwallet/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gilardh/myhushwallet/precache-manifest.f6f139e9fd36a71c895710833d4ecfb1.js"
+  "/myhushwallet/precache-manifest.cdec032b0d11456823d627772f9287ee.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/gilardh/myhushwallet/index.html", {
+workbox.routing.registerNavigationRoute("/myhushwallet/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
