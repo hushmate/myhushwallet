54adac188c687aaf59233173f28220defc3fe353
gilardh <gilardh@yahoo.com>
2018-12-26 20:37:02 +0100

    Little modification of the presentation

--- src/components/pages/Faq.jsx
+++ src/components/pages/Faq.jsx
@@ -1,45 +1,61 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 
+const ColoredLine = ({ color }) => (
+  <hr
+      style={{
+          color: color,
+          backgroundColor: color,
+          height: 0.25
+      }}
+  />
+);
+
+const styles = {
+  normal: {
+    fontWeight: 'normal'
+  },
+  bold: {
+    fontWeight: 'bold'
+  },
+};
+
 export default class ZFaq extends React.Component {
   render() {
     return (
       <Container>
         <Row>
           <Col>
-          <h2>Faq</h2>
-            <p>
-              <b>Q: Are my private keys secured?</b>
-              <br/>
-              A: Yes they are secured, your private keys never leave your browser.
-            </p>
-
-            <p>
-              <b>Q: How is this secure? Its on a web browser!</b>
-              <br/>
-              A: My Hush Wallet has replicated the core features of the HUSH
-              daemon in JavaScript! So the process of creating and signing the
-              transactions are all done within the browser. No sensitive
-              information is sent through the network.
-            </p>
-
-            <p>
-              <b>Q: Can I have the source code?</b>
-              <br/>
-              A:{" "}
-              <a href="https://github.com/myhush/myhushwallet">Here you go</a>
-            </p>
-
-            <p>
-              <b>Q: Why are you doing this?</b>
-              <br/>
-              A: My Hush Wallet was inspired by{" "}
-              <a href="https://myzenwallet.io/">MyZenWallet's</a> which was
-              inspired by{" "}
-              <a href="https://myetherwallet.com">MyEtherWallet's.</a> We also
-              want to provide the people the ability to interact with the HUSH
-              blockchain easily, without having to run a full node.
-            </p>
+          <h1>FAQ</h1>
+          <ColoredLine color="grey" />
+            <br/>
+              <p>
+                <div style={styles.bold}>Are my private keys secured?</div>
+                <div style={styles.normal}>Yes they are secured, your private keys never leave your browser.</div>
+              </p>
+            <br/>
+              <p>
+              <div style={styles.bold}>How is this secure? Its on a web browser!</div>
+              <div style={styles.normal}>My Hush Wallet has replicated the core features of the HUSH daemon in JavaScript!</div>
+              <div style={styles.normal}>So the process of creating and signing the transactions are all done within the browser.</div>
+              <div style={styles.normal}>No sensitive information is sent through the network.</div>
+              </p>
+            <br/>
+              <p>
+                <div style={styles.bold}>Can I have the source code?</div>
+                {" "}
+                <a href="https://github.com/myhush/myhushwallet">Here you go</a>
+              </p>
+            <br/>
+              <p>
+              <div style={styles.bold}>Why are you doing this?</div>
+                My Hush Wallet was inspired by{" "}
+                <a href="https://myzenwallet.io/">MyZenWallet's</a> which was
+                inspired by{" "}
+                <a href="https://myetherwallet.com">MyEtherWallet's.</a> We also
+                want to provide the people the ability to interact with the HUSH
+                blockchain easily, without having to run a full node.
+              </p>
           </Col>
         </Row>
       </Container>
